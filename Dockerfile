d690851047fc42f8d6be9d65741824c4417fb320
Alexander Vödisch <alexander.voedisch@mailbox.org>
2020-03-14 17:22:33 +0100

    init

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,8 @@ RUN mkdir /opt/tmserver
 
 WORKDIR /opt/tmserver
 
-COPY TrackmaniaServer_2011-02-21.zip /opt/tmserver
-RUN apt-get update && apt-get install -y unzip
-RUN unzip /opt/tmserver/TrackmaniaServer_2011-02-21.zip -d /opt/tmserver
-COPY custom_game_settings.txt /opt/tmserver/GameData/Tracks/MatchSettings/
+COPY server /opt/tmserver
 COPY RunTrackmaniaServer.sh /opt/tmserver/
-
-ARG SERVER_NAME='Trackmania Server'
-ARG SERVER_DESC='This is a Trackmania Server'
-ARG SERVER_SA_PASSWORD='SuperAdmin'
-ARG SERVER_ADM_PASSWORD='Admin'
-ENV SERVER_NAME $SERVER_NAME
-ENV SERVER_DESC $SERVER_DESC
-ENV SERVER_SA_PASSWORD $SERVER_SA_PASSWORD
-ENV SERVER_ADM_PASSWORD $SERVER_ADM_PASSWORD
 
 EXPOSE 5000/tcp
 EXPOSE 2350/tcp
--- RunTrackmaniaServer.sh
+++ RunTrackmaniaServer.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-echo "Setting ENV/ARG variables"
-sed -i "s/<password>SuperAdmin/<password>${SERVER_SA_PASSWORD}/" /opt/tmserver/GameData/Config/dedicated_cfg.txt
-sed -i "s/<password>Admin/<password>${SERVER_ADM_PASSWORD}/" /opt/tmserver/GameData/Config/dedicated_cfg.txt
-sed -i "s/<name></<name>${SERVER_NAME}</" /opt/tmserver/GameData/Config/dedicated_cfg.txt
-sed -i "s/<comment></<comment>${SERVER_DESC}</" /opt/tmserver/GameData/Config/dedicated_cfg.txt
-sed -i "s/<xmlrpc_allowremote>False/<xmlrpc_allowremote>True/" /opt/tmserver/GameData/Config/dedicated_cfg.txt
-
-echo "Server config dedicated_cfg.txt is"
-cat /opt/tmserver/GameData/Config/dedicated_cfg.txt
-
 echo "Launching Server"
-exec ./TrackmaniaServer /dedicated_cfg=dedicated_cfg.txt /game_settings=MatchSettings/custom_game_settings.txt /nodaemon /lan
+exec ./TrackmaniaServer /dedicated_cfg=dedicated_cfg.txt /game_settings=MatchSettings/custom_game_settings.txt /nodaemon /online /verbose_rpc_full
