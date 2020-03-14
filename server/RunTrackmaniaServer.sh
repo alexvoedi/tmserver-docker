bfeb7b67f3e5dcd95bd44f8dd5253a450a501e52
Alexander Vödisch <alexander.voedisch@mailbox.org>
2020-03-14 17:58:11 +0100

    fix

--- server/RunTrackmaniaServer.sh
+++ server/RunTrackmaniaServer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Launching Server"
-exec ./TrackmaniaServer /dedicated_cfg=dedicated_cfg.txt /game_settings=MatchSettings/custom_game_settings.txt /nodaemon
+exec ./TrackmaniaServer /dedicated_cfg=dedicated_cfg.txt /game_settings=MatchSettings/custom_game_settings.txt /nodaemon /online
